3a5391541f8b020c0a44a6c9491c27d097e8120d
hrz123 <2403076194@qq.com>
2020-04-18 16:56:46 +0800

    :sparkles: NEW: add codes to crawl beijing showing-soon movies

---
+++ crawler.py
@@ -0,0 +1,38 @@
+# crawler.py
+
+import time
+
+import requests
+from bs4 import BeautifulSoup
+
+
+def main():
+    headers = {
+        "Cookie":     "bid=\"qNzQ6uMlaCw\"",
+        "User-Agent": "Paw/3.1.10 (Macintosh; OS X/10.15.4) GCDHTTPRequest"
+    }
+    url = "https://movie.douban.com/cinema/later/beijing/"
+    init_page = requests.get(url, headers=headers).content
+    init_soup = BeautifulSoup(init_page, 'lxml')
+
+    all_movies = init_soup.find('div', id="showing-soon")
+    for each_movie in all_movies.find_all('div', class_="item"):
+        all_a_tag = each_movie.find_all('a')
+        all_li_tag = each_movie.find_all('li')
+
+        movie_name = all_a_tag[1].text
+        url_to_fetch = all_a_tag[1]['href']
+        movie_date = all_li_tag[0].text
+
+        response_item = requests.get(url_to_fetch, headers=headers).content
+        soup_item = BeautifulSoup(response_item, 'lxml')
+        img_tag = soup_item.find('img')
+
+        print('{} {} {}'.format(movie_name, movie_date, img_tag['src']))
+
+
+if __name__ == '__main__':
+    s = time.perf_counter()
+    main()
+    e = time.perf_counter()
+    print(e - s)
---
+++ python_callback.py
@@ -0,0 +1,24 @@
+# python_callback.py
+import asyncio
+import time
+
+
+async def crawl_page(url):
+    print('crawling {}'.format(url))
+    sleep_time = int(url.split('_')[-1])
+    await asyncio.sleep(sleep_time)
+    return 'OK {}'.format(url)
+
+
+async def main(urls):
+    tasks = [asyncio.create_task(crawl_page(url)) for url in urls]
+    for task in tasks:
+        task.add_done_callback(lambda future: print('result: ', future.result()))
+    await asyncio.gather(*tasks)
+
+
+if __name__ == '__main__':
+    s = time.perf_counter()
+    asyncio.run(main(['url_1', 'url_2', 'url_3', 'url_4']))
+    e = time.perf_counter()
+    print(e - s)
